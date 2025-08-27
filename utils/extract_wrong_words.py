8cc883e766b894557c9a0d06f9609e15bd09fa68
MartaVidas123 <mv53134@fer.hr>
2025-08-27 11:48:17 +0200

    fix

--- utils/extract_wrong_words.py
+++ utils/extract_wrong_words.py
@@ -71,6 +71,7 @@ def evaluate_and_find_errors(model_name, settings_args, model_args, device):
                     pred = int(pred)
                     gold = int(gold)
                     if pred != gold:
+                        print(words[i])
                         errors_val.append({
                             "token": words[i],
                             "predicted": num_to_tag[pred],
