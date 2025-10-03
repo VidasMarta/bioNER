ee1b6463c295727a6be3f57cfedeb1aff55f3eea
MartaVidas123 <mv53134@fer.hr>
2025-10-03 17:47:49 +0200

    changed to bioLinkBERT embedding model

--- train.py
+++ train.py
@@ -134,7 +134,7 @@ if __name__ == "__main__":
     #logger.calculate_mean_stddev()
 
     #---> Extract wrongly classified entities from validation and test sets (modelName = "D1_ftB_C_L_5_A_with_gen2Data")
-    errors_val, errors_test= evaluate_and_find_errors(model_name, settings_args, model_args, device = torch.device("cuda" if torch.cuda.is_available() else "cpu"))
+    """errors_val, errors_test= evaluate_and_find_errors(model_name, settings_args, model_args, device = torch.device("cuda" if torch.cuda.is_available() else "cpu"))
     
 
     with open(output_path+'/wrong_val.log',"w") as f:
@@ -144,10 +144,10 @@ if __name__ == "__main__":
 
     with open(output_path+'/wrong_test.log',"w") as f:
         for error in errors_test:
-            f.write(f"{error} \n")
+            f.write(f"{error} \n")"""
 
     #---> Train with generated data (with 5 different seeds - commented)
-    """model_name, model_args, settings_args = extract_args()
+    model_name, model_args, settings_args = extract_args()
     output_path = os.path.join(settings.LOG_PATH, model_name)
     logger = Logger(output_path, model_args, settings_args)
 
@@ -172,7 +172,7 @@ if __name__ == "__main__":
         
     
     logger.calculate_mean_stddev()
-    logger_2.calculate_mean_stddev()"""
+    logger_2.calculate_mean_stddev()
     
 
 
