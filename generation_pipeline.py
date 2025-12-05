03daa5c588b9095fefd1a09c8ad1379068164145
MartaVidas123 <mv53134@fer.hr>
2025-12-05 13:02:00 +0100

    refactor

--- generation_pipeline.py
+++ generation_pipeline.py
@@ -16,7 +16,7 @@ import matplotlib.pyplot as plt
 
 def argparse_args():
     parser = argparse.ArgumentParser(description="LLM-based text Generator iteration pipeline with k-shot.")
-    parser.add_argument('--config_file', type=str, default='~/syn-bioner/bioNER/experiments/default_generate.yml', help='Path to config file with all arguments.')
+    parser.add_argument('--config_file', type=str, default='/home/mvidas/syn-bioner/bioNER/experiments/default_generate.yml', help='Path to config file with all arguments.')
 
     return parser.parse_args()
 
