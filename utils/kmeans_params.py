45dfe70c13fe34bd01a64e44a55d157462466b69
MartaVidas123 <mv53134@fer.hr>
2025-12-09 11:15:46 +0100

    fix

--- utils/kmeans_params.py
+++ utils/kmeans_params.py
@@ -7,7 +7,7 @@ from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 from sklearn.preprocessing import normalize
 from typing import List, Dict, Any
-import parsing_embedding as pe
+import utils.parsing_embedding as pe
 import os
 
 def load_syntax_data(path: str) -> List[Dict[str, Any]]:
