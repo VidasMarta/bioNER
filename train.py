b269bdf5d9d17abf2aca6989d85a4050c25c8a29
MartaVidas123 <mv53134@fer.hr>
2026-02-04 12:12:57 +0100

    changed config for trainig

--- train.py
+++ train.py
@@ -167,14 +167,14 @@ if __name__ == "__main__":
         set_seed(seed)
         #First train on gen data
         model_name_seed = model_name + f"_seed{seed}"
-        main(model_name_seed, model_args, settings_args, logger)
+        #main(model_name_seed, model_args, settings_args, logger)
 
         #Then take weights and finetune on NCBI-disease train set
         model_args_2 = copy.deepcopy(model_args)
         model_args_2['weights'] = torch.load(settings.MODEL_PATH + f"/{model_name_seed}_best.bin")
         settings_args_2 = copy.deepcopy(settings_args)
         settings_args_2['dataset'] = 'ncbi_disease_json'
-        settings_args_2['train_filename'] = 'ncbi_ner_train_10pct.json' #'train.json'
+        settings_args_2['train_filename'] = 'ncbi_ner_train_20pct.json' #'train.json'
 
         main(new_model_name, model_args_2, settings_args_2, logger_2)
         
