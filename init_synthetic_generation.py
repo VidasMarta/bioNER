c1e3f6a260adff9cef8e3adfffc218c65469cb2b
kmihak <kebermiha@gmail.com>
2026-02-04 15:49:00 +0100

    bug

--- init_synthetic_generation.py
+++ init_synthetic_generation.py
@@ -113,24 +113,26 @@ def main(args: argparse.Namespace) -> None:
     os.makedirs(args.output_directory, exist_ok=True)
     # open json file where each line is one dict
     term_list = utils.generate_term_list(args.disease_file, args.verbose)
+    
+    if args.obo_file_path:
+        disease_terms = get_diseases(args)
+        term_list += disease_terms
     # TODO: pairs and triplets of terms.
     if args.pairs_file_path:
         disease_terms = utils.generate_term_list(args.pairs_file_path, args.verbose)
         term_list += disease_terms
-    # print(term_list[:150])
+
     if args.test:
         term_list = term_list[:2] + term_list[400:406] + term_list[1100:1102] + term_list[-2:]
         print("Testing on samples: ", len(term_list), term_list)
     random.seed(args.random_seed)
-    term_list = random.sample(term_list, args.generate_k)
-
-    nlp = generation_postprocessing.spacy_load_model('en_core_web_trf')
-    # TODO: system_template, user_template to args
-    generate_sentence_samples(args, term_list, 
-                              system_template='role_sent_type_prompt',
-                              user_template='genre_new_prompt',
-                              nlp = nlp)
-
+    if os.path.isfile(os.path.join(args.output_directory, 'term_list.txt')):
+        with open(os.path.join(args.output_directory, 'term_list.txt'), 'r') as f:
+            used_terms = [line.strip() for line in f.readlines()]
+        term_list = list(set(term_list) - set(used_terms))
+    if len(term_list) > args.generate_k:
+        term_list = random.sample(term_list, args.generate_k)
+        
 
 if __name__ == "__main__":
     init_args = argparse_args()
