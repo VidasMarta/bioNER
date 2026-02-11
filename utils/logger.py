2c747b713e506fbd78e1bde2519056d6cf9527d1
MartaVidas123 <mv53134@fer.hr>
2026-02-11 14:03:41 +0100

    added mean precision and recall to logger print

--- utils/logger.py
+++ utils/logger.py
@@ -41,6 +41,8 @@ class Logger:
 
     def calculate_mean_stddev(self):
         f1_scores = []
+        precission_scores = []
+        recall_scores = []
         f1_strict_scores = []
 
         with open(self.output_path + '/test.log', 'r') as file:
@@ -50,6 +52,16 @@ class Logger:
                     f1_end = line.index(",", f1_start)
                     f1 = float(line[f1_start:f1_end].strip())
                     f1_scores.append(f1)
+
+                    p_start = line.index("precison:") + len("precison:")
+                    p_end = line.index(",", p_start)
+                    p = float(line[p_start:p_end].strip())
+                    precission_scores.append(p)
+
+                    r_start = line.index("recall:") + len("recall:")
+                    r_end = line.index(",", r_start)
+                    r = float(line[r_start:r_end].strip())
+                    recall_scores.append(f1)
 
                     f1s_start = line.index("f1_score(strict):") + len("f1_score(strict):")
                     f1s_end = line.index(",", f1s_start)
@@ -63,7 +75,13 @@ class Logger:
         f1_strict_mean = np.mean(f1_strict_scores)
         f1_strct_std = np.std(f1_strict_scores)
 
-        mean_std = f"Mean f1_score: {f1_mean}, Std. dev. f1_score: {f1_std}, Mean f1_score (strict): {f1_strict_mean}, Std. dev. f1_score (strict): {f1_strct_std}"
+        p_mean = np.mean(precission_scores)
+        p_std = np.std(precission_scores)
+
+        r_mean = np.mean(recall_scores)
+        r_std = np.std(recall_scores)
+
+        mean_std = f"Mean f1_score: {f1_mean}, Std. dev. f1_score: {f1_std}, Mean f1_score (strict): {f1_strict_mean}, Std. dev. f1_score (strict): {f1_strct_std}\n Mean precision: {p_mean}, Std. dev precision: {p_std}, Mean recall: {r_mean}, Std. dev recall: {r_std}"
 
         log_file = open(self.output_path + '/test.log',"a")
         log_file.write(mean_std)
