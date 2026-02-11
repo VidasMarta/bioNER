61123a4b88a3ba4617045e5127ea6749e840083e
MartaVidas123 <mv53134@fer.hr>
2026-02-11 13:51:06 +0100

    changed train

--- train.py
+++ train.py
@@ -125,7 +125,7 @@ def print_args(model_args, settings_args):
 
 if __name__ == "__main__":    
     #---> Train normal with 5 different seeds
-    model_name, model_args, settings_args = extract_args()
+    '''model_name, model_args, settings_args = extract_args()
     print_args(model_args, settings_args)
     output_path = os.path.join(settings.LOG_PATH, model_name)
     logger = Logger(output_path, model_args, settings_args)
@@ -135,7 +135,7 @@ if __name__ == "__main__":
         main(model_name, model_args, settings_args, logger)
         
     
-    logger.calculate_mean_stddev()
+    logger.calculate_mean_stddev()'''
     """
 
     #---> Extract wrongly classified entities from validation and test sets (modelName = "D1_ftB_C_L_5_A_with_gen2Data")
@@ -151,16 +151,15 @@ if __name__ == "__main__":
         for error in errors_test:
             f.write(f"{error} \n")"""
 
-    '''
     #---> Train with generated data (with 5 different seeds - commented)
     model_name, model_args, settings_args = extract_args()
     settings_args['dataset'] = 'synthetic'
-    settings_args['train_filename'] = 'corrected_generated_sentences_20260109.jsonl'
+    settings_args['train_filename'] = 'corrected_generated_sentences_20260205.jsonl'
     print_args(model_args, settings_args)
     output_path = os.path.join(settings.LOG_PATH, model_name)
     logger = Logger(output_path, model_args, settings_args)
 
-    new_model_name = "S_ftB_C_L_5_A_with_S1_50NCBIft" #D1_ftB_C_L_5_A_mean_with_genData
+    new_model_name = "S_ftB_C_L_5_A_with_S2_10NCBIft" #D1_ftB_C_L_5_A_mean_with_genData
     output_path = os.path.join(settings.LOG_PATH, new_model_name)
     logger_2 = Logger(output_path)
 
@@ -176,14 +175,14 @@ if __name__ == "__main__":
         model_args_2['weights'] = torch.load(settings.MODEL_PATH + f"/{model_name_seed}_best.bin")
         settings_args_2 = copy.deepcopy(settings_args)
         settings_args_2['dataset'] = 'ncbi_disease_json'
-        settings_args_2['train_filename'] = 'ncbi_ner_train_50pct.json' #'train.json'
+        settings_args_2['train_filename'] = 'ncbi_ner_train_10pct.json' #'train.json'
 
         main(new_model_name, model_args_2, settings_args_2, logger_2)
         
         
     
     logger.calculate_mean_stddev()
-    logger_2.calculate_mean_stddev()'''
+    logger_2.calculate_mean_stddev()
     
 
 
