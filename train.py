f81056f4d57dd97b93250662f5296d4e0fe7200f
MartaVidas123 <mv53134@fer.hr>
2026-03-03 18:25:00 +0100

    train config change

--- train.py
+++ train.py
@@ -127,7 +127,7 @@ def print_args(model_args, settings_args):
 
 if __name__ == "__main__":    
     #---> Train normal with 5 different seeds
-    model_name, model_args, settings_args = extract_args()
+    '''model_name, model_args, settings_args = extract_args()
     settings_args['dataset'] = 'k_shot/10_pct/'
     settings_args['train_filename'] = 'corrected_generated_sentences_20260302.jsonl'
     print_args(model_args, settings_args)
@@ -140,7 +140,7 @@ if __name__ == "__main__":
         main(model_name, model_args, settings_args, logger)
         
     
-    logger.calculate_mean_stddev()
+    logger.calculate_mean_stddev()'''
     """
 
     #---> Extract wrongly classified entities from validation and test sets (modelName = "D1_ftB_C_L_5_A_with_gen2Data")
@@ -157,14 +157,14 @@ if __name__ == "__main__":
             f.write(f"{error} \n")"""
 
     #---> Train with generated data (with 5 different seeds - commented)
-    '''model_name, model_args, settings_args = extract_args()
+    model_name, model_args, settings_args = extract_args()
     settings_args['dataset'] = 'k_shot/10_pct/'
     settings_args['train_filename'] = 'corrected_generated_sentences_20260302.jsonl'
     print_args(model_args, settings_args)
     output_path = os.path.join(settings.LOG_PATH, model_name)
     logger = Logger(output_path, model_args, settings_args)
 
-    new_model_name = "S2_C_L_5_A_with_NCBI_ft" #D1_ftB_C_L_5_A_mean_with_genData
+    new_model_name = "kshot_10pct_C_L_5_A_with_10NCBI_ft" #D1_ftB_C_L_5_A_mean_with_genData
     output_path = os.path.join(settings.LOG_PATH, new_model_name)
     logger_2 = Logger(output_path)
 
@@ -183,14 +183,14 @@ if __name__ == "__main__":
         model_args_2['weights'] = torch.load(settings.MODEL_PATH + f"/{model_name_seed}_best.bin")
         settings_args_2 = copy.deepcopy(settings_args)
         settings_args_2['dataset'] = 'ncbi_disease_json/trf'
-        settings_args_2['train_filename'] = 'train.json' # 'train.json', train_50pct.json
+        settings_args_2['train_filename'] = 'train_10pct.json' # 'train.json', train_50pct.json
 
         main(new_model_name, model_args_2, settings_args_2, logger_2)
         
         
     
     logger.calculate_mean_stddev()
-    logger_2.calculate_mean_stddev()'''
+    logger_2.calculate_mean_stddev()
     
 
 
