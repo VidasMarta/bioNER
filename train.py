92b5ec7ce563afb86707a491f0e7e2cd9d2e6001
MartaVidas123 <mv53134@fer.hr>
2026-03-21 15:13:05 +0100

    added csv for table metrics

--- train.py
+++ train.py
@@ -139,8 +139,9 @@ if __name__ == "__main__":
         new_model_name = model_name + f"_seed{seed}"
         main(new_model_name, model_args, settings_args, logger)
         
-    
-    logger.calculate_mean_stddev()
+    #trainig_set - treba biti koje ime će u tablici taj model imati
+    #file_to_save grupirati na koliko pct dataseta se trenira ili finetune-a model
+    logger.calculate_mean_stddev(training_set="-", file_to_save=os.path.join(settings.LOG_PATH, "distemist/100_pct/test.csv"))
     """
 
     #---> Extract wrongly classified entities from validation and test sets (modelName = "D1_ftB_C_L_5_A_with_gen2Data")
@@ -157,20 +158,20 @@ if __name__ == "__main__":
             f.write(f"{error} \n")"""
 
     #---> Train with generated data (with 5 different seeds - commented)
-    model_name, model_args, settings_args = extract_args()
-    settings_args['dataset'] = 'k_shot/100_pct/'
-    settings_args['train_filename'] = 'corrected_generated_sentences_20260304.jsonl'
+    '''model_name, model_args, settings_args = extract_args()
+    model_name = "ES_S_with_0shot"
+    settings_args['dataset'] = 'distemist/trf/synthetic/0shot/'
+    settings_args['train_filename'] = 'corrected_generated_sentences_20260316.jsonl'
     print_args(model_args, settings_args)
     output_path = os.path.join(settings.LOG_PATH, model_name)
     logger = Logger(output_path, model_args, settings_args)
 
-    new_model_name = "kshot_100pct_C_L_5_A_with_NCBI_ft" #D1_ftB_C_L_5_A_mean_with_genData
+    new_model_name = "0shot_ft_with_10DISTEMIST" #D1_ftB_C_L_5_A_mean_with_genData
     output_path = os.path.join(settings.LOG_PATH, new_model_name)
     logger_2 = Logger(output_path)
 
     for seed in [42, 198, 6000, 3828, 7382]:
-        set_seed(seed)
-        #First train on gen data #TODO na Speku preimenuj seed model names da ne ide na 
+        set_seed(seed) 
         model_name_seed = model_name + f"_seed{seed}" 
         if not os.path.exists(settings.MODEL_PATH + f"/{model_name_seed}_best.bin"):
             model_args['weights'] = None
@@ -182,15 +183,13 @@ if __name__ == "__main__":
         model_args_2 = copy.deepcopy(model_args)
         model_args_2['weights'] = torch.load(settings.MODEL_PATH + f"/{model_name_seed}_best.bin")
         settings_args_2 = copy.deepcopy(settings_args)
-        settings_args_2['dataset'] = 'ncbi_disease_json/trf'
-        settings_args_2['train_filename'] = 'train.json' # 'train.json', train_50pct.json
+        settings_args_2['dataset'] = 'distemist/trf/'
+        settings_args_2['train_filename'] = 'train_10pct.json' # 'train.json', train_50pct.json
 
         main(new_model_name, model_args_2, settings_args_2, logger_2)
         
-        
-    
-    logger.calculate_mean_stddev()
-    logger_2.calculate_mean_stddev()
+    logger.calculate_mean_stddev(training_set="0-shot", file_to_save=os.path.join(settings.LOG_PATH, "distemist/0shot/test.csv"))
+    logger_2.calculate_mean_stddev(training_set="3-shot\_10pct", file_to_save=os.path.join(settings.LOG_PATH, "distemist/10_pct/test.csv"))'''
     
 
 
--- utils/logger.py
+++ utils/logger.py
@@ -2,6 +2,7 @@ import os
 import json
 from datetime import datetime
 import numpy as np
+import pandas as pd
 
 class Logger:
     def __init__(self, model_logs_path, model_args=None, settings_args=None):
@@ -14,6 +15,7 @@ class Logger:
         open(self.output_path+'/train.log',"a").close()
         open(self.output_path+'/valid.log',"a").close()
         open(self.output_path+'/test.log',"a").close()
+
 
         #write config (hyperparameters etc.)
         log_config_file = open(self.output_path+'/config.log', "w")
@@ -39,7 +41,7 @@ class Logger:
         log_file.write(f"epoch: {epoch}, loss: {loss}, time: {datetime.now() - self.init_time}\n")
         log_file.close()
 
-    def calculate_mean_stddev(self):
+    def calculate_mean_stddev(self, training_set, file_to_save):
         f1_scores = []
         precission_scores = []
         recall_scores = []
@@ -80,6 +82,22 @@ class Logger:
 
         r_mean = np.mean(recall_scores)
         r_std = np.std(recall_scores)
+
+        new_data = {
+            'Training set': training_set,
+            'F1': f"{f1_mean*100:0.2f} +/- {f1_std*100:0.2f}",
+            'Precision': f"{p_mean*100:0.2f} +/- {p_std*100:0.2f}",
+            'Recall': f"{r_mean*100:0.2f} +/- {r_std*100:0.2f}"
+        }
+
+        if os.path.exists(file_to_save):
+            df = pd.read_csv(file_to_save)
+            df = pd.concat([df, pd.DataFrame([new_data])], ignore_index=True)
+        else:
+            df = pd.DataFrame([new_data])
+            os.makedirs(os.path.dirname(file_to_save), exist_ok=True)
+
+        df.to_csv(file_to_save, index=False)
 
         mean_std = f"Mean f1_score: {f1_mean}, Std. dev. f1_score: {f1_std}, Mean f1_score (strict): {f1_strict_mean}, Std. dev. f1_score (strict): {f1_strct_std}\n Mean precision: {p_mean}, Std. dev precision: {p_std}, Mean recall: {r_mean}, Std. dev recall: {r_std}"
 
