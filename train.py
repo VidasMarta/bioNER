90e59120228dc0f112faa7688e02c7f95e8fe247
MartaVidas123 <mv53134@fer.hr>
2026-03-25 09:32:07 +0100

    train config change

--- train.py
+++ train.py
@@ -127,8 +127,8 @@ def print_args(model_args, settings_args):
 
 if __name__ == "__main__":    
     #---> Train normal with 5 different seeds
-    model_name, model_args, settings_args = extract_args()
-    settings_args['dataset'] = 'distemist/synthetic/kshot_20pct/'
+    '''model_name, model_args, settings_args = extract_args()
+    settings_args['dataset'] = 'distemist/synthetic/kshot_50pct/'
     settings_args['train_filename'] = 'corrected_generated_sentences_20260318.jsonl' #train_10pct.json
     print_args(model_args, settings_args)
     output_path = os.path.join(settings.LOG_PATH, model_name)
@@ -141,7 +141,7 @@ if __name__ == "__main__":
         
     #trainig_set - treba biti koje ime će u tablici taj model imati
     #file_to_save grupirati na koliko pct dataseta se trenira ili finetune-a model
-    logger.calculate_mean_stddev(training_set="-", file_to_save=os.path.join(settings.LOG_PATH, "distemist/kshot/20pct/test.csv"))
+    logger.calculate_mean_stddev(training_set="-", file_to_save=os.path.join(settings.LOG_PATH, "distemist/kshot/50pct/test.csv"))'''
     """
 
     #---> Extract wrongly classified entities from validation and test sets (modelName = "D1_ftB_C_L_5_A_with_gen2Data")
@@ -158,15 +158,15 @@ if __name__ == "__main__":
             f.write(f"{error} \n")"""
 
     #---> Train with generated data (with 5 different seeds - commented)
-    '''model_name, model_args, settings_args = extract_args()
+    model_name, model_args, settings_args = extract_args()
     model_name = "ES_S_with_0shot"
-    settings_args['dataset'] = 'distemist/trf/synthetic/0shot/'
-    settings_args['train_filename'] = 'corrected_generated_sentences_20260316.jsonl'
+    settings_args['dataset'] = 'distemist/trf/synthetic/kshot_20pct/'
+    settings_args['train_filename'] = 'corrected_generated_sentences_20260318.jsonl'
     print_args(model_args, settings_args)
     output_path = os.path.join(settings.LOG_PATH, model_name)
     logger = Logger(output_path, model_args, settings_args)
 
-    new_model_name = "0shot_ft_with_10DISTEMIST" #D1_ftB_C_L_5_A_mean_with_genData
+    new_model_name = "kshot_20pct_ft_with_10DISTEMIST" #D1_ftB_C_L_5_A_mean_with_genData
     output_path = os.path.join(settings.LOG_PATH, new_model_name)
     logger_2 = Logger(output_path)
 
@@ -188,8 +188,8 @@ if __name__ == "__main__":
 
         main(new_model_name, model_args_2, settings_args_2, logger_2)
         
-    logger.calculate_mean_stddev(training_set="0-shot", file_to_save=os.path.join(settings.LOG_PATH, "distemist/0shot/test.csv"))
-    logger_2.calculate_mean_stddev(training_set="3-shot\_10pct", file_to_save=os.path.join(settings.LOG_PATH, "distemist/10_pct/test.csv"))'''
+    #logger.calculate_mean_stddev(training_set="0-shot", file_to_save=os.path.join(settings.LOG_PATH, "distemist/0shot/test.csv"))
+    logger_2.calculate_mean_stddev(training_set="3-shot\_20pct", file_to_save=os.path.join(settings.LOG_PATH, "distemist/10_pct/test.csv"))
     
 
 
